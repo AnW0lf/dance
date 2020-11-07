d488a6f9d55eb390843a3d9302ce6e8ace457718
SergeyVolkov <sergeyvolkovapplife@users.noreply.github.com>
2020-11-07 14:29:43 +0300

    music

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource = null;
+    [SerializeField] private AudioClip[] _tracks;
 
+    private AudioClip _currentTrack;
     public bool Active { get; private set; } = false;
 
     public void Play()
     {
+        _currentTrack = _tracks[(int)Random.Range(0f, _tracks.Length)];
+        _audioSource.clip = _currentTrack;
+
         Active = true;
         _audioSource.Play();
     }
