47023b91f448da8170049bf637e3876e0e422d18
SergeyVolkov <sergeyvolkovapplife@users.noreply.github.com>
2020-11-08 22:03:17 +0300

    something

---
+++ Assets/WishSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WishSwitcher : MonoBehaviour
+{
+    public bool wishEnabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!wishEnabled)
+        {
+            GetComponentInChildren<FanController>()._chancePerSecond = 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
