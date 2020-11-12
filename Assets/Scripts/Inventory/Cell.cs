2f21b093d0f680a4e8753afca0a4746a293c2522
AnW0lf <AngryWolfgang@gmail.com>
2020-11-12 17:15:23 +0300

    inventory system

---
+++ Assets/Scripts/Inventory/Cell.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Inventory
+{
+    public class Cell : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+    {
+        [SerializeField] private bool _isEmpty = false;
+        [SerializeField] private Image _background = null;
+        [SerializeField] private Image _icon = null;
+        [SerializeField] private TextMeshProUGUI _label = null;
+        [SerializeField] private Cell _draggedCell = null;
+
+        public Dance Dance { get; private set; } = null;
+
+        public bool IsEmpty => Dance == null;
+
+        public Cell DraggedCell
+        {
+            get => _draggedCell;
+            set => _draggedCell = value;
+        }
+
+        public static Cell TargetCell { get; set; } = null;
+
+        public void SetDance(Dance dance)
+        {
+            if (dance == null) Clear();
+            else
+            {
+                Dance = dance;
+
+                _background.sprite = Dance.BackgroundSprite;
+                _icon.sprite = Dance.IconSprite;
+                _label.text = Dance.LabelText;
+
+                _background.gameObject.SetActive(true);
+                _icon.gameObject.SetActive(true);
+                _label.gameObject.SetActive(true);
+            }
+        }
+
+        public void Clear()
+        {
+            Dance = null;
+
+            _background.sprite = null;
+            _icon.sprite = null;
+            _label.text = string.Empty;
+
+            _background.gameObject.SetActive(false);
+            _icon.gameObject.SetActive(false);
+            _label.gameObject.SetActive(false);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            TargetCell = this;
+
+            DraggedCell.SetDance(Dance);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            DraggedCell.transform.position = eventData.position;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (TargetCell != null)
+            {
+                TargetCell = null;
+                DraggedCell.Clear();
+            }
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            if (TargetCell != this)
+            {
+                Dance dance = TargetCell.Dance;
+                TargetCell.SetDance(Dance);
+                SetDance(dance);
+                TargetCell = null;
+                DraggedCell.Clear();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/Storage.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Inventory
+{
+    public class Storage : MonoBehaviour, IDropHandler
+    {
+        [SerializeField] private bool _addCell = false;
+        [SerializeField] private Transform _cellContainer = null;
+        [SerializeField] private GameObject _cellPrefab = null;
+        [SerializeField] private Cell _draggedCell = null;
+        [SerializeField] private List<Dance> _dances = null;
+        private List<Cell> _cells = new List<Cell>();
+
+        public void AddCell(Dance dance)
+        {
+            Cell cell = Instantiate(_cellPrefab, _cellContainer).GetComponent<Cell>();
+            cell.SetDance(dance);
+            cell.DraggedCell = _draggedCell;
+            _cells.Add(cell);
+        }
+
+        public void RemoveCell(Cell cell)
+        {
+            _cells.Remove(cell);
+            Destroy(cell.gameObject);
+        }
+
+        private void Update()
+        {
+            for (int i = _cells.Count - 1; i >= 0; i--)
+                if (_cells[i].IsEmpty) RemoveCell(_cells[i]);
+
+            if (_addCell)
+            {
+                _addCell = false;
+                AddCell(_dances[Random.Range(0, _dances.Count)]);
+            }
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            if (_cells.Contains(Cell.TargetCell)) return;
+            Dance dance = Cell.TargetCell.Dance;
+            Cell.TargetCell.DraggedCell.Clear();
+            Cell.TargetCell.Clear();
+            Cell.TargetCell = null;
+            AddCell(dance);
+        }
+    }
+}
