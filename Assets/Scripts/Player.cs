696879bbc64e9b4d5ddc4e2e0d6ad968224ef465
AnW0lf <AngryWolfgang@gmail.com>
2020-11-16 19:20:52 +0300

    fix

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance { get; private set; } = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this) Destroy(gameObject);
+    }
+
+    private int _classicFansCount = 0;
+    private int _jazzFansCount = 0;
+    private int _streetFansCount = 0;
+
+    private int _money = 0;
+
+    private List<Dance> _storage = null;
+    private List<Dance> _asset = null;
+}
