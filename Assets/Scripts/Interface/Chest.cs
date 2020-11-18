f325230dc8b5d1d55ebdd75101f903fa6715e16f
AnW0lf <AngryWolfgang@gmail.com>
2020-11-18 17:39:06 +0300

    Chests

---
+++ Assets/Scripts/Interface/Chest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private int _moneyReward = 0;
+    [SerializeField] private int _classicFanReward = 0;
+    [SerializeField] private int _jazzFanReward = 0;
+    [SerializeField] private int _streetFanReward = 0;
+    [SerializeField] private Image _fade = null;
+    [SerializeField] private float _fadingSpeed = 2f;
+    [SerializeField] private RewardMiniGame _miniGame = null;
+
+    public void Reward()
+    {
+        if (_miniGame.KeyCount <= 0) return;
+        _miniGame.KeyCount--;
+        _miniGame.UpdateLabel();
+        Player.Instance.Money += _moneyReward;
+        Player.Instance.ClassicFansCount += _classicFanReward;
+        Player.Instance.JazzFansCount += _jazzFanReward;
+        Player.Instance.StreetFansCount += _streetFanReward;
+        _fade.raycastTarget = false;
+        StartCoroutine(Fading(0f));
+    }
+
+    private IEnumerator Fading(float alpha)
+    {
+        float startAlpha = _fade.color.a;
+        float timer = 0f;
+        float duration = Mathf.Abs(startAlpha - alpha) / _fadingSpeed;
+        while (timer <= duration)
+        {
+            timer += Time.deltaTime;
+            Color color = _fade.color;
+            color.a = Mathf.Lerp(startAlpha, alpha, timer / duration);
+            _fade.color = color;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Interface/RewardMiniGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.UI;
+
+public class RewardMiniGame : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _label = null;
+    [SerializeField] private string _statement = string.Empty;
+    [SerializeField] private int _keyCount = 3;
+
+    public int KeyCount { get => _keyCount; set => _keyCount = value; }
+
+    public void UpdateLabel()
+    {
+        _label.text = string.Format(_statement, _keyCount);
+    }
+}
