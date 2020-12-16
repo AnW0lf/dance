2190901ca5282e780ab45d4ff187c8e60bf94162
AnW0lf <AngryWolfgang@gmail.com>
2020-12-17 01:13:44 +0300

    spend indicator

---
+++ Assets/Scripts/Interface/ShopIndicator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Interface
+{
+    [RequireComponent(typeof(Image))]
+    public class ShopIndicator : MonoBehaviour
+    {
+        private Image _image = null;
+
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+            CanSpend = Player.Instance.Price <= Player.Instance.Money;
+            Player.Instance.OnMoneyChanged += (money) => CanSpend = Player.Instance.Price <= money;
+            Player.Instance.OnPriceChanged += (price) => CanSpend = price <= Player.Instance.Money;
+        }
+
+        private bool _canSpend = false;
+        public bool CanSpend
+        {
+            get => _canSpend;
+            set 
+            {
+                if (_canSpend != value)
+                {
+                    _canSpend = value;
+                    _image.enabled = _canSpend;
+                }
+            }
+        }
+    }
+}
